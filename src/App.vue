61a9562ff827de894125df9cd364c22762c5bf6b
IqroNegoro <iqronegoro0@gmail.com>
2024-01-16 15:57:16 +0700

    pushing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="overflow-hidden bg-primary w-full h-screen">
-    <img src="/img/textures.png" alt="" class="fixed top-0 left-0 w-full h-full mix-blend-color-dodge z-40">
+  <div class="md:overflow-hidden overflow-x-hidden bg-primary w-full md:h-screen">
+    <!-- <img src="/img/textures.png" alt="" class="fixed top-0 left-0 w-full h-full mix-blend-color-dodge z-40"> -->
     <Navbar />
     <router-view class="relative z-10" />
-    <div class="blur-[100px] bg-red-500 fixed -top-24 -right-24 w-64 h-64 rounded-full"></div>
-    <div class="blur-[100px] bg-red-500 fixed top-96 -left-24 w-64 h-64 rounded-full"></div>
+    <!-- <div class="blur-[100px] bg-red-500 fixed -top-24 -right-24 w-64 h-64 rounded-full"></div>
+    <div class="blur-[100px] bg-red-500 fixed top-96 -left-24 w-64 h-64 rounded-full"></div> -->
   </div>
 </template>
 <script setup>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-row justify-center items-center w-full h-screen p-24 pt-0 pb-0">
+    <div class="flex flex-col md:flex-row justify-center items-center w-full md:h-screen p-8 md:p-24 pt-0 pb-0">
         <div class="w-full flex flex-col justify-center items-start gap-8 h-full">
             <h1 class="text-7xl font-black text-white leading-[100px]">JUJUTSU KAISEN 2ND SEASON</h1>
             <div class="flex flex-row justify-center items-center gap-8 w-3/4">
@@ -14,78 +14,68 @@
             </div>
             <button class="flex border hover:bg-orange transition-colors duration-150 border-orange px-4 py-1 text-sm justify-center items-center text-white">Watch Jujutsu Kaisen <i class='bx bx-up-arrow-alt rotate-45 text-3xl'></i></button>
         </div>
-        <div class="relative w-full h-full p-12 pb-0">
-            <div class="border -z-10 border-white p-4 pb-0 h-full w-96 absolute inset-8 bg-gradient-to-r from-white/5 to-transparent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3">
+        <div class="relative w-full max-md:h-screen md:p-12 md:pb-0 max-md:order-first flex justify-center items-center">
+            <div class="border -z-10 border-white p-4 pb-0 md:h-full h-3/4 w-80 md:w-96 absolute inset-8 bg-gradient-to-r from-white/5 to-transparent top-24 md:top-1/2 left-1/2 -translate-x-1/2 translate-0 md:-translate-y-1/3">
                 <img class="w-full h-full" src="/img/Rectangle 1.png">
                 <img src="/img/star.png" alt="" class="absolute -top-12 -left-[51px]">
                 <i class="bx bx-search absolute top-8 right-8 text-xl border-2 border-orange px-2 py-1 text-white rounded-full"></i>
             </div>
+            <img v-show="i == 0" src="/img/gojo.png" alt="" class="w-full max-md:scale-[1.375]">
+            <img v-show="i == 1" src="/img/geto.png" alt="" class="w-full max-md:scale-[1.425]">
+            <img v-show="i == 2" src="/img/shoko.png" alt="" class="w-full max-md:scale-[1.275]">
+            <img v-show="i == 3" src="/img/riko.png" alt="" class="w-full max-md:scale-[1.425]">
+            <img v-show="i == 4" src="/img/toji.png" alt="" class="w-full max-md:scale-[1.45]">
             <Transition name="pop-up">
-                <img v-show="i == 0" src="/img/gojo.png" alt="" class="w-full">
-            </Transition>
-            <Transition name="pop-up">
-                <img v-show="i == 1" src="/img/geto.png" alt="" class="w-full">
-            </Transition>
-            <Transition name="pop-up">
-                <img v-show="i == 2" src="/img/shoko.png" alt="" class="w-full">
-            </Transition>
-            <Transition name="pop-up">
-                <img v-show="i == 3" src="/img/riko.png" alt="" class="w-full">
-            </Transition>
-            <Transition name="pop-up">
-                <img v-show="i == 4" src="/img/toji.png" alt="" class="w-full">
-            </Transition>
-            <Transition name="pop-up">
-                <div v-show="i == 0" class="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
+                <div v-show="i == 0" class="absolute w-full top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
                     <p>五条悟</p>
                     <p class="font-black text-3xl">GOJO SATORU</p>
-                    <button class="bg-[#FF8718]/75 border-l border-white tet-white px-3">
+                    <button class="bg-[#FF8718]/75 max-md:text-xl border-l border-white text-white px-3">
                         <span class="font-bold">VA:</span> Nakamura Yuuichi
                     </button>
                 </div>
             </Transition>
             <Transition name="pop-up">
-                <div v-show="i == 1" class="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
+                <div v-show="i == 1" class="absolute w-full top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
                     <p>夏油傑</p>
                     <p class="font-black text-3xl">GETOU SUGURU</p>
-                    <button class="bg-[#FF8718]/75 border-l border-white tet-white px-3">
+                    <button class="bg-[#FF8718]/75 max-md:text-xl border-l border-white text-white px-3">
                         <span class="font-bold">VA:</span> Sakurai Takahiro
                     </button>
                 </div>
             </Transition>
             <Transition name="pop-up">
-                <div v-show="i == 2" class="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
+                <div v-show="i == 2" class="absolute w-full top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
                     <p>家入硝子</p>
                     <p class="font-black text-3xl">SHOKO IEIRI</p>
-                    <button class="bg-[#FF8718]/75 border-l border-white tet-white px-3">
+                    <button class="bg-[#FF8718]/75 max-md:text-xl border-l border-white text-white px-3">
                         <span class="font-bold">VA:</span> Endou Aya
                     </button>
                 </div>
             </Transition>
             <Transition name="pop-up">
-                <div v-show="i == 3" class="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
+                <div v-show="i == 3" class="absolute w-full top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
                     <p>天内理子</p>
                     <p class="font-black text-3xl">RIKO AMANAI</p>
-                    <button class="bg-[#FF8718]/75 border-l border-white tet-white px-3">
+                    <button class="bg-[#FF8718]/75 max-md:text-xl border-l border-white text-white px-3">
                         <span class="font-bold">VA:</span> Nagase Anna
                     </button>
                 </div>
             </Transition>
             <Transition name="pop-up">
-                <div v-show="i == 4" class="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
+                <div v-show="i == 4" class="absolute w-full top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
                     <p>伏黒甚爾</p>
                     <p class="font-black text-3xl">TOJI FUSHIGURO</p>
-                    <button class="bg-[#FF8718]/75 border-l border-white tet-white px-3">
+                    <button class="bg-[#FF8718]/75 max-md:text-xl border-l border-white text-white px-3">
                         <span class="font-bold">VA:</span> Koyasu Takehito
                     </button>
                 </div>
             </Transition>
-            <div class="z-50 text-white absolute w-80 flex justify-between items-center top-1/2 left-1/2 -translate-x-1/2 translate-y-48">
+            <div class="z-[99] text-white absolute w-80 flex justify-between items-center top-1/2 left-1/2 -translate-x-1/2 translate-y-48">
                 <button @click="i <= 0 ? i = 4 : i--"><i class="bx bx-arrow-back text-3xl"></i></button>
                 <button @click="i >= 4 ? i = 0 : i++"><i class="bx bx-arrow-back text-3xl bx-rotate-180"></i></button>
             </div>
         </div>
-        <div class="w-[120%] fixed -bottom-12 -left-40 h-36 flex flex-col flex-nowrap rotate-6">
+        <div class="w-[120%] max-md:hidden fixed -bottom-12 -left-40 h-36 flex flex-col flex-nowrap rotate-6">
             <div class="w-full marquee h-full bg-[#FF8718] text-black font-bold flex flex-row justify-center items-center text-3xl gap-12">
                 <p>LATEST SEASON</p>
                 <img src="/img/stridgestar.png" alt="">
