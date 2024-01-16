fdb467cdaf880d075e9728f33198798f23480fd7
IqroNegoro <iqronegoro0@gmail.com>
2024-01-16 13:09:31 +0700

    stridge

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,17 +15,39 @@
             <button class="flex border hover:bg-orange transition-colors duration-150 border-orange px-4 py-1 text-sm justify-center items-center text-white">Watch Jujutsu Kaisen <i class='bx bx-up-arrow-alt rotate-45 text-3xl'></i></button>
         </div>
         <div class="relative w-full h-full p-12 pb-0">
-            <img src="/img/gojo.png" alt="" class="w-full">
-            <div class="border -z-10 border-white p-4 pb-0 h-full w-96 absolute inset-8 bg-gradient-to-r from-white/5 to-transparent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3">
-                <img class="w-full h-full" src="/img/Rectangle 1.png">
-                <img src="/img/star.png" alt="" class="absolute -top-12 -left-[51px]">
-                <i class="bx bx-search absolute top-8 right-8 text-xl border-2 border-orange px-2 py-1 text-white rounded-full"></i>
-            </div>
-            <div class="absolute flex justify-center items-center top-0 left-0 w-full h-full">
-                <div class="flex flex-col justify-center items-center text-xl text-white">
+            <div>
+                <img src="/img/gojo.png" alt="" class="w-full">
+                <div class="border -z-10 border-white p-4 pb-0 h-full w-96 absolute inset-8 bg-gradient-to-r from-white/5 to-transparent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3">
+                    <img class="w-full h-full" src="/img/Rectangle 1.png">
+                    <img src="/img/star.png" alt="" class="absolute -top-12 -left-[51px]">
+                    <i class="bx bx-search absolute top-8 right-8 text-xl border-2 border-orange px-2 py-1 text-white rounded-full"></i>
+                </div>
+                <div class="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center items-center gap-3 text-xl text-white">
                     <p>五条悟</p>
                     <p class="font-black">GOJO SATORU</p>
+                    <button class="bg-[#FF8718]/75 border-l border-white tet-white px-3">
+                        <span class="font-bold">VA:</span> Nakamura Yuuichi
+                    </button>
                 </div>
+            </div>
+            <div class="absolute w-full left-0 top-96">
+
+            </div>
+        </div>
+        <div class="w-[120%] fixed -bottom-12 -left-40 h-36 flex flex-col flex-nowrap rotate-6">
+            <div class="w-full h-full bg-[#FF8718] text-black font-bold flex flex-row justify-center items-center text-3xl gap-12">
+                <p>LATEST SEASON</p>
+                <img src="/img/stridgestar.png" alt="">
+                <p>BEST ACTION ANIME</p>
+                <img src="/img/stridgestar.png" alt="">
+                <p>JUJUTSU KAISEN 2ND SEASON</p>
+            </div>
+            <div class="w-full h-full text-black font-bold flex flex-row justify-center items-center text-3xl gap-12">
+                <p>TOP RATED ANIME</p>
+                <img src="/img/stridgestar.png" alt="">
+                <p>RECOMMENDATION</p>
+                <img src="/img/stridgestar.png" alt="">
+                <p>JUJUTSU KAISEN 2ND SEASON</p>
             </div>
         </div>
     </div>
